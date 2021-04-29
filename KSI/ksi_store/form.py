1ee9beb04acf3eec94ed69bc1b5afba53878edf2
smit53 <70460904+smit53@users.noreply.github.com>
2021-04-29 12:17:54 +0530

    Create form.py

---
+++ KSI/ksi_store/form.py
@@ -0,0 +1,63 @@
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder" : "Username",                
+                "class": "form-control"
+            }
+        ))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder" : "Password",                
+                "class": "form-control"
+            }
+        ))
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "placeholder" : "Username",                
+                "class": "form-control"
+            }
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder" : "Email",                
+                "class": "form-control"
+            }
+        ))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder" : "Password",                
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder" : "Password check",                
+                "class": "form-control"
+            }
+        ))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+
+class ContactForm(forms.Form):
+    name= forms.CharField(max_length=500, label="name")
+    email= forms.EmailField(max_length=500, label="email")
+    subject= forms.CharField(max_length=150, label="subject")
+    message= forms.CharField(label='',widget=forms.Textarea(
+                        attrs={'placeholder': 'message'}))
